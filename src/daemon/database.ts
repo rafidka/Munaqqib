bd3031d6f04cd8fcd323905940716033babc10a6
Rafid K. Al-Humaimidi <rafidka@gmail.com>
2018-09-10 21:14:16 -0700

    Enforce strict typing (strict: true) in tsconfig.json

--- src/daemon/database.ts
+++ src/daemon/database.ts
@@ -1,4 +1,5 @@
 import Sequelize from "sequelize";
+import { Error } from "tslint/lib/error";
 
 const sequelize = new Sequelize("database", "username", "password", {
   dialect: "sqlite",
@@ -17,13 +18,11 @@ const sequelize = new Sequelize("database", "username", "password", {
   operatorsAliases: false
 });
 
-sequelize.authenticate();
-
 class ServiceAttributes {
   id?: number;
-  url: string;
-  indexName: string;
-  typeName: string;
+  url?: string;
+  indexName?: string;
+  typeName?: string;
   lastFetch?: Date;
 }
 
@@ -33,60 +32,76 @@ type ServiceModel = Sequelize.Model<ServiceInstance, ServiceAttributes>;
 class ServiceRequestAttributes {
   id?: number;
   serviceId?: number;
-  httpStatusCode: number;
+  httpStatusCode?: number;
 }
 
 type ServiceRequestInstance = Sequelize.Instance<ServiceRequestAttributes> & ServiceRequestAttributes;
 type ServiceRequestModel = Sequelize.Model<ServiceRequestInstance, ServiceRequestAttributes>;
 
-async function createServiceTable(db: Database) {
-  db.services = sequelize.define<ServiceInstance, ServiceAttributes>("service",
-    {
-      id: {type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true},
-      url: {type: Sequelize.STRING, allowNull: false},
-      indexName: {type: Sequelize.STRING, allowNull: false},
-      typeName: {type: Sequelize.STRING, allowNull: false},
-      lastFetch: {type: Sequelize.DATE, allowNull: true},
-    });
-  await db.services.sync();
-}
-
-async function createServiceRequestTable(db: Database) {
-  db.serviceRequests = sequelize.define<ServiceRequestInstance, ServiceRequestAttributes>("serviceRequest",
-    {
-      id: {type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true},
-      httpStatusCode: {type: Sequelize.INTEGER, allowNull: false},
-    });
-  db.serviceRequests.belongsTo(db.services);
-  await db.serviceRequests.sync();
-}
-
-interface Database {
-  services?: ServiceModel;
-  serviceRequests?: ServiceRequestModel;
+class Database {
+  private _services?: ServiceModel;
+  private _serviceRequests?: ServiceRequestModel;
   creationPromise?: Promise<void>;
+
+  public constructor() {
+    this.creationPromise = new Promise((resolve, reject) => {
+      this.createModels().then(function () {
+        console.info("Database created.");
+        resolve();
+      }, function () {
+        console.error("Failed to create database.");
+        reject();
+      }).catch(function () {
+        console.error("Failed to create database.");
+        reject();
+      });
+    });
+  }
+
+  public get services(): ServiceModel {
+    if (!this._services) {
+      throw new Error("this._services is undefined.");
+    }
+    return this._services;
+  }
+
+  public get serviceRequests(): ServiceRequestModel {
+    if (!this._serviceRequests) {
+      throw new Error("this._serviceRequests is undefined.");
+    }
+    return this._serviceRequests;
+  }
+
+  private async createModels() {
+    await this.createServiceModel();
+    await this.createServiceRequestModel();
+  }
+
+  private async createServiceModel() {
+    this._services = sequelize.define<ServiceInstance, ServiceAttributes>("service",
+      {
+        id: {type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true},
+        url: {type: Sequelize.STRING, allowNull: false},
+        indexName: {type: Sequelize.STRING, allowNull: false},
+        typeName: {type: Sequelize.STRING, allowNull: false},
+        lastFetch: {type: Sequelize.DATE, allowNull: true},
+      });
+    await this._services.sync();
+  }
+
+  private async createServiceRequestModel() {
+    if (!this._services) {
+      throw new Error("this._services is undefined. createServiceModel() should be called before calling this function.");
+    }
+    this._serviceRequests = sequelize.define<ServiceRequestInstance, ServiceRequestAttributes>("serviceRequest",
+      {
+        id: {type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true},
+        httpStatusCode: {type: Sequelize.INTEGER, allowNull: false},
+      });
+    this._serviceRequests.belongsTo(this._services);
+    await this._serviceRequests.sync();
+  }
+
 }
 
-async function createTables(db: Database) {
-  await createServiceTable(db);
-  await createServiceRequestTable(db);
-}
-
-const db: Database = {
-};
-
-db.creationPromise = new Promise((resolve, reject) => {
-  createTables(db).then(function() {
-    console.info("Database created.");
-    resolve();
-  }, function() {
-    console.error("Failed to create database.");
-    reject();
-  }).catch(function() {
-    console.error("Failed to create database.");
-    reject();
-  });
-});
-
-
-export = db;
+export = new Database();
